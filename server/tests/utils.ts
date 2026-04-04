2081c1fdbb4f96252eb25a387de2441899496063
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-04 21:10:16 +0300

    feat: add basic helper utils like inserting admin account in database.

---
+++ server/tests/utils.ts
@@ -0,0 +1,30 @@
+import { AdminRepo, app } from "../src/lib.js";
+import { default as supertest } from "supertest";
+import { v4 as uuidv4 } from "uuid";
+import { default as argon2 } from "argon2";
+
+export const server = supertest(app);
+
+export class AdminAccount {
+  username: string;
+  passwordHash: string;
+  pswd: string;
+
+  constructor(name: string, pswd: string, pswdhash: string) {
+    this.passwordHash = pswdhash;
+    this.pswd = pswd;
+    this.username = name;
+  }
+}
+
+export async function insertAdmin(): Promise<AdminAccount> {
+  let username = uuidv4();
+  let pswdhash = await argon2.hash(username);
+  let adac = new AdminAccount(username, username, pswdhash);
+  let ad = AdminRepo.create({
+    username: username,
+    passwordHash: pswdhash,
+  });
+  await AdminRepo.insert(ad);
+  return adac;
+}
