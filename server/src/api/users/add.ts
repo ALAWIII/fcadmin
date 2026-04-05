13297a288fe5bd6940aa503f746819092f2f5c15
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-05 13:17:57 +0300

    feat: update addUser endpoint handler logic to handle errors and missing values.

--- server/src/api/users/add.ts
+++ server/src/api/users/add.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from "express";
 import { default as argon2 } from "argon2";
-import { dbCon } from "../../db.js";
+import { dbCon, userRepo } from "../../db.js";
 import { v4 as uuidv4 } from "uuid";
 import { CreateBucketCommand } from "@aws-sdk/client-s3";
 import { Folder, User } from "../../models.js";
 import { rfs } from "../../lib.js";
+import { logger } from "../../telemetry.js";
 
 interface UserAdd {
   email: string;
@@ -14,11 +15,22 @@ interface UserAdd {
 
 export async function addUser(req: Request, res: Response) {
   const { email, username, password } = req.body as UserAdd;
-  const passwordHash = await argon2.hash(password);
+
+  if (!email || !username || !password || password.length < 4) {
+    res.status(400).json({
+      error:
+        "email, username and password are required, or password length < 4",
+    });
+    return;
+  }
+  logger.info({ email, username }, "creating new user account");
   const userId = uuidv4();
   const folderId = uuidv4();
   try {
+    logger.info("hashing password");
+    const passwordHash = await argon2.hash(password);
     // Atomic DB transaction
+    logger.info("starting atomic database transaction.");
     await dbCon.transaction(async (manager) => {
       // 1. Create user (no root_folder yet)
       await manager.insert(User, {
@@ -38,22 +50,27 @@ export async function addUser(req: Request, res: Response) {
       // 3. Link root folder back to user
       await manager.update(User, { id: userId }, { rootFolder: folderId });
     });
-  } catch (err) {
-    res.status(500).json({ error: `Failed to create user: ${err}` });
-    return;
+    logger.info("retrieving user info.");
+    const user = await userRepo.findOne({
+      where: { id: userId },
+    });
+    res.status(201).json(user);
+  } catch (err: any) {
+    if (err.code === "23505") {
+      logger.error("duplicate user.");
+      res.status(409).json({ error: "Username or email already taken" });
+      return;
+    }
+    logger.error({ err }, "Failed to create user");
+    res.status(500).json({ error: "Failed to create user" });
   }
-
+  //------------------ bucket creation----
   try {
     await rfs.send(new CreateBucketCommand({ Bucket: userId }));
   } catch (err) {
+    await userRepo.delete({ id: userId });
     // S3 failed — rollback DB manually
-    await dbCon.transaction(async (manager) => {
-      await manager.delete(Folder, { id: folderId });
-      await manager.delete(User, { id: userId });
-    });
     res.status(500).json({ error: "Failed to create storage bucket" });
     return;
   }
-
-  res.status(201).json({ message: "User created" });
 }
