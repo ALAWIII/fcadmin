db5d0b9ab52a94141bf243a2b7a7c18ec8102c06
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-06 13:47:01 +0300

    test: add list endpoint tests.

--- server/tests/users/list.ts
+++ server/tests/users/list.ts
@@ -1,4 +1,28 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
+import { addUser, insertValidAdmin, server } from "../utils.js";
+import { v4 } from "uuid";
+import type { User } from "../../src/models.js";
 
-async function listUsers() {}
-test(() => {});
+async function listUsers() {
+  const admin = await insertValidAdmin();
+
+  let uArr: User[] = [];
+  for (let i = 0; i < 3; i++) {
+    const uInfo = v4();
+    let userF = {
+      email: `${uInfo}@shawarma.com`,
+      password: uInfo,
+      username: uInfo,
+    };
+    uArr.push(await addUser(userF));
+  }
+
+  let resp = await server
+    .get("/api/user/list")
+    .auth(admin.jwt!, { type: "bearer" })
+    .expect(200);
+  let usersL = resp.body as User[];
+  expect(usersL).containSubset(uArr);
+  expect(usersL.findIndex((u) => u.id === uArr[0]?.id)).not.toEqual(-1);
+}
+test("list all users.", listUsers);
