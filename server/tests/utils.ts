05abf77f609ac481bbc55d472f71eeeeaa58655d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-06 16:05:36 +0300

    test: add some file upload and query helpers.

--- server/tests/utils.ts
+++ server/tests/utils.ts
@@ -1,8 +1,14 @@
-import { AdminRepo, app, rdsCon } from "../src/lib.js";
+import { AdminRepo, app, fileRepo, rdsCon } from "../src/lib.js";
 import { default as supertest } from "supertest";
 import { v4 as uuidv4 } from "uuid";
 import { default as argon2 } from "argon2";
 import type { User } from "../src/models.js";
+import {
+  HeadObjectCommand,
+  PutObjectCommand,
+  type S3Client,
+} from "@aws-sdk/client-s3";
+import { expect } from "vitest";
 
 export const server = supertest(app);
 
@@ -52,4 +58,43 @@ export async function addUser(user: NewUser) {
     .expect(201);
   rdsCon.setex(`refresh:${user.username}`, 60 * 60, "");
   return resp.body as User;
+}
+
+async function uploadRfsFile(con: S3Client, bucket: string, key: string) {
+  let cmd = new PutObjectCommand({ Bucket: bucket, Key: key, Body: "j" });
+  return await con.send(cmd);
+}
+export async function injectFile(
+  id: string,
+  ownerId: string,
+  parentId: string,
+  rfsCon: S3Client,
+) {
+  let f = await fileRepo.save({
+    id,
+    ownerId,
+    parentId,
+    name: "shawarma",
+    size: 2,
+    etag: "notImportant",
+    mimeType: "notImportant",
+  });
+  let resp = await uploadRfsFile(rfsCon, ownerId, id);
+  expect(resp.Size).toBeDefined;
+  return f;
+}
+export async function rfsObjectExists(
+  rfsCon: S3Client,
+  bucket: string,
+  key: string,
+): Promise<boolean> {
+  try {
+    await rfsCon.send(new HeadObjectCommand({ Bucket: bucket, Key: key }));
+    return true;
+  } catch (e: any) {
+    if (e.name === "NotFound" || e.$metadata?.httpStatusCode === 404) {
+      return false;
+    }
+    throw e; // re-throw unexpected errors
+  }
 }
