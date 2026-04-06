008bdad05e793528227b819b0a264a3c28a2fe64
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-06 16:05:53 +0300

    test: implement remove endpoint tests.

--- server/tests/users/remove.ts
+++ server/tests/users/remove.ts
@@ -1,3 +1,51 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
+import {
+  addUser,
+  rfsObjectExists,
+  injectFile,
+  insertValidAdmin,
+  server,
+} from "../utils.js";
+import { v4 } from "uuid";
+import rfsCon from "../../src/rustfs.js";
+import { fileRepo, userRepo } from "../../src/db.js";
 
-test(() => {});
+async function removeUser() {
+  const admin = await insertValidAdmin();
+  let uInfo = v4();
+  let u = await addUser({
+    email: `${uInfo}@shawarma.com`,
+    password: uInfo,
+    username: uInfo,
+  });
+  let fid = v4();
+  let fResp = await injectFile(fid, u.id, u.rootFolder!, rfsCon);
+  let fBefore = await fileRepo.findOneBy({ id: fid });
+  expect(fBefore).not.toBeNull();
+  let resp = await server
+    .delete(`/api/user/remove/${u.id}`)
+    .auth(admin.jwt!, { type: "bearer" })
+    .expect(200);
+  let body = resp.body;
+  expect(body.message as string).toEqual("User deleted");
+  let fAfter = await fileRepo.findOneBy({ id: fid });
+  let uAfter = await userRepo.findOneBy({ id: u.id });
+  let fexists = await rfsObjectExists(rfsCon, u.id, fid);
+  expect(fexists).toEqual(false);
+  expect(fAfter).toBeNull();
+  expect(uAfter).toBeNull();
+}
+
+async function removeNonExistedUser() {
+  const admin = await insertValidAdmin();
+  let uid = v4();
+  let resp = await server
+    .delete(`/api/user/remove/${uid}`)
+    .auth(admin.jwt!, { type: "bearer" })
+    .expect(404);
+  let body = resp.body;
+  expect(body.error as string).toEqual("User not found");
+}
+
+test("remove user with all its files/folders.", removeUser);
+test("remove nonexisted user.", removeNonExistedUser);
