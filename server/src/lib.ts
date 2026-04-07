2e5458f93e2c30805e6cf2fa8ab400ae5ca0cddb
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-07 12:07:31 +0300

    refactor: remove unneeded import.

--- server/src/lib.ts
+++ server/src/lib.ts
@@ -3,5 +3,5 @@ export { default as rdsCon } from "./redis.js";
 export { default as rfs } from "./rustfs.js";
 export { default as app } from "./app.js";
 import "./api/admin.js";
-import "./api/users/mod.js";
+import "./api/mod.js";
 import { logger } from "./telemetry.js";
