4659a2faa13516026d5e8e5af14b4e14cda536a9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-12 15:34:24 +0300

    recreate ui/ structure using official command template.

--- ui/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["tests/**/*.ts"], // any .ts file inside tests/
-    exclude: ["tests/utils.ts"],
-  },
-});
