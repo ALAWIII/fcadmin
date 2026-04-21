0e3ed54b65737e95bb12af3d109f75e326246ce6
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-21 15:21:25 +0300

    refactor: remove unneaded imports.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,6 @@
-import { app, logger } from "./lib.js";
+import app from "./index.js";
+import { logger } from "./telemetry.js";
+
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
