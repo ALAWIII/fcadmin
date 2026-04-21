d07bcd4964c08f59aca0eba4fed2a268f3f567f2
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-21 15:13:13 +0300

    feat: add loging to middleware auth handlers.

--- server/src/middleware.ts
+++ server/src/middleware.ts
@@ -1,17 +1,24 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import settings from "./config.js";
+import { logger } from "./telemetry.js";
 // server/src/middleware.ts
 
+/**
+ used to protect and redirect user to login page when he doesnot provide the token or when he accesses a private endpoint.
+*/
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies?.token;
+  logger.debug({ path: req.path, token: token }, "start requireAuth process.");
 
   if (!token) return res.redirect("/");
 
   try {
     jwt.verify(token, settings.secrets.hmac);
+    logger.debug("token verified successfully.");
     next();
   } catch {
+    logger.error("invalid or missing token redirect to login page.");
     res.clearCookie("token");
     return res.redirect("/");
   }
@@ -30,18 +37,24 @@ declare global {
   }
 }
 
+/**
+ * auth middleware to extract token from headers and cookies.
+ */
 export function auth(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies?.token ?? req.headers.authorization?.split(" ")[1]; // "Bearer <token>"
-
+  logger.debug({ token: token, path: req.path }, "start authenticating admin.");
   if (!token) {
+    logger.error({ token: token }, "invalid or missing token.");
     res.status(401).json({ error: "No token provided" });
     return;
   }
 
   try {
     req.user = jwt.verify(token, settings.secrets.hmac) as JwtPayload;
+    logger.debug("token verified.");
     next();
   } catch (err: any) {
+    logger.error({ error: err }, "unexpected authentication error.");
     const msg =
       err.name === "TokenExpiredError" ? "Token expired" : "Invalid token";
     res.status(401).json({ error: msg });
